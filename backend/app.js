75306992d9fe66deeed40c5f3ecfe7136c20516b
Unknown <cherif.mohamed86@gmail.com>
2019-03-25 18:42:46 +0100

    3.41

--- backend/app.js
+++ backend/app.js
@@ -6,25 +6,29 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+
 app.post('/api/posts', (req, res, next) => {
   const post = req.body;
-  console.log();
+  console.log(post);
   res.status(201).json({
     message: 'post added successfully'
   })
 })
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Header",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
-  next();
-});
-
-app.use('/api/posts', (req, res, next) => {
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     {
     id: 'ffaj12',
